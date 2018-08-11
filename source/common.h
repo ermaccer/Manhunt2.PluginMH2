d3606047873f6cac99b0207cbbe9456845849f9a
ermaccer <40604575+ermaccer@users.noreply.github.com>
2018-08-11 19:25:45 +0200

    Add files via upload

---
+++ source/common.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+
+
+// plugin variables
+
+int KeyMH2BaseScreenshotMode;
+int KeyMH2EnableScreenshotMode;
+int KeyMH2DisableScreenshotMode;
+int KeyMH2ShowCoordinates;
+bool MH2NoLegalScreen;
+bool MH2ForceRatsToAppear;
+bool MH2EnableScreenshotMode;
+
+// weird address, can glitch stuff
+// TODO: find a better one 
+int& DisableHudAndFreezeWorld = *(int*)0x78949C;
+int& FreeCamera = *(int*)0x7894A4;
+
+float& MH2PlayerX = *(float *)0x704F68;
+float& MH2PlayerY = *(float *)0x704F70;
+float& MH2PlayerZ = *(float *)0x704F6C;
+
+
+
+// Functions
+void(__cdecl *WriteDebug)(int, char*);
+
+
+bool KeyHit(unsigned int keyCode) 
+{
+	return (GetKeyState(keyCode) & 0x8000) != 0;
+}
+
+void DisplayPlayerCoords()
+{
+	char buffer[64];
+	sprintf_s(buffer, "X = %f Y = %f Z = %f", MH2PlayerX, MH2PlayerY, MH2PlayerZ);
+	WriteDebug(10, buffer);
+}
+
---
+++ source/dllmain.cpp
@@ -0,0 +1,77 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+// standalone from PluginMH
+#include "..\shared\stdafx.h"
+#include "..\shared\IniReader.h"
+#include "..\shared\MemoryMgr.h"
+#include "common.h"
+
+using namespace Memory::VP;
+void GetFuncs()
+{
+	WriteDebug = (void(__cdecl *)(int,char*))0x53D8D0;
+}
+
+void GetIniValues()
+{
+	CIniReader reader("PluginMH2.ini");
+	KeyMH2ShowCoordinates = reader.ReadInteger("Settings", "KeyMH2ShowCoordinates", 0x74);
+	KeyMH2BaseScreenshotMode = reader.ReadInteger("Settings", "KeyMH2BaseScreenshotMode", 0x11);
+	KeyMH2EnableScreenshotMode = reader.ReadInteger("Settings", "KeyMH2EnableScreenshotMode", 0x56);
+	KeyMH2DisableScreenshotMode = reader.ReadInteger("Settings", "KeyMH2DisableScreenshotMode", 0x42);
+	MH2ForceRatsToAppear = reader.ReadBoolean("Settings", "ForceRatsToAppear", 0);
+	MH2NoLegalScreen = reader.ReadBoolean("Settings", "DisableLegalScreen", 0);
+	MH2EnableScreenshotMode = reader.ReadBoolean("Settings", "EnableScreenshotMode", 0);
+}
+
+void WINAPI Init()
+{
+	GetFuncs();
+	GetIniValues();
+	Sleep(10);
+		while (true) {
+			if (KeyHit(KeyMH2ShowCoordinates)) DisplayPlayerCoords();
+			// levels requires rat paths? they appear in good ending without this 
+			// patch
+			if (MH2ForceRatsToAppear) Patch(0x7942B4, 32);
+
+			if (MH2NoLegalScreen)
+			{
+				Patch(0x53FC68, 0);
+				Patch(0x53FC50, 0);
+				Patch(0x53FC6F, 0);
+			}
+
+			//TODO: Disable need for Disable/Enable key
+			if (MH2EnableScreenshotMode)
+			{
+				if (KeyHit(KeyMH2BaseScreenshotMode) && KeyHit(KeyMH2EnableScreenshotMode))
+				{
+					
+					FreeCamera = 1; DisableHudAndFreezeWorld = 1;
+				}
+				if (KeyHit(KeyMH2BaseScreenshotMode) && KeyHit(KeyMH2DisableScreenshotMode))
+				{
+
+					FreeCamera = 0; DisableHudAndFreezeWorld = 0;
+				}
+			}
+			Sleep(1);
+		}
+}
+
+
+
+BOOL WINAPI DllMain( HMODULE hModule, DWORD  ul_reason_for_call,LPVOID lpReserved)
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	{
+		if (*(int*)0x63BC93 != (unsigned int)'hnaM')
+		{
+			MessageBoxA(0, "Invalid executable!", 0, 0);
+			return -1;
+		}
+		else CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(Init), nullptr, 0, nullptr);
+	}
+	return TRUE;
+}
+
